8b26a7abc5e65c2b6e59df49fb2f214348c777a5
vsecco <vsecco@ciandt.com>
2018-06-21 21:42:54 -0300

    Atualizado mecanismo de rotas, criado componentes login e home e exemplo de navegaçao entre rotas

--- frontend/drools-soo-front/src/app/app-routing.module.ts
+++ frontend/drools-soo-front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [ RouterModule ],
   declarations: []
 })
--- frontend/drools-soo-front/src/app/app.module.ts
+++ frontend/drools-soo-front/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     NgbModule.forRoot(),
