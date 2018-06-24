3e4e3d45858f2aac7bffb1fcd31466cb0f1243fa
Larissa Rodrigues <lrodrigues@ciandt.com>
2018-06-23 23:03:47 -0300

    versionando repository

---
+++ backend/src/main/java/unesp/rc/creditloan/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package unesp.rc.creditloan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import unesp.rc.creditloan.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
